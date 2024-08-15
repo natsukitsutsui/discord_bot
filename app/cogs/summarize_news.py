5d60630f2739d758b8b2b1f2b2b235efb469a09b
Natsuki Tsutsui <natsuki.tsutsui.08@gmail.com>
2024-08-16 01:14:50 +0900

    進捗

--- app/cogs/summarize_news.py
+++ app/cogs/summarize_news.py
@@ -2,3 +2,14 @@ import discord
 from openai import OpenAI
 from discord.ext import commands
 
+class SummarizeNewsCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="news")
+    def summarize_news(self, ctx):
+        if ctx.author == self.bot.user:
+            return
+
+        if ctx.channel.id != int(os.getenv("COMMANDS_CHANNEL_ID")):
+            return
